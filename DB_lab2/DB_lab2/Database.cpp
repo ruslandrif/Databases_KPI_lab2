3ffa182ddfe858f67203e1230cf27f0caaff54d1
ruslandrif <ruslandrif93@gmail.com>
2021-11-13 22:02:49 +0200

    Remove password

--- DB_lab2/DB_lab2/Database.cpp
+++ DB_lab2/DB_lab2/Database.cpp
@@ -11,47 +11,12 @@
 
 int main()
 {
-    auto model = std::make_shared<model::CDatabaseModel>("postgres", "IT-company", "reussite54321");
+    auto model = std::make_shared<model::CDatabaseModel>("postgres", "IT-company", "passw");
     auto view = std::make_shared<view::CDatabaseView>(model);
 
     auto controller = std::make_shared<controller::CDatabaseController>(model, view);
 
     auto choose = controller->requestAction();
     controller->performAction(choose);
-    //const std::string connection_string =
-    //    "user=postgres password=reussite54321 host = 127.0.0.1 dbname= postgres";
-    //PGconn* conn = PQconnectdb(connection_string.c_str());
-    ///* Check to see that the backend connection was successfully made */
-    //if (PQstatus(conn) != CONNECTION_OK) {
-    //    std::cout << "Connection to database failed: " << PQerrorMessage(conn)
-    //        << std::endl;
-    //    PQfinish(conn);
-    //    return 1;
-    //}
-    //else {
-    //    std::cout << "Connection to database succeed." << std::endl;
-    //}
-
-    //auto* res = PQexec(conn, "select * from phonebook;");
-    //if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-    //    std::cout << "Select failed: " << PQresultErrorMessage(res) << std::endl;
-    //}
-    //else {
-    //    std::cout << "Get " << PQntuples(res) << "tuples, each tuple has "
-    //        << PQnfields(res) << "fields" << std::endl;
-    //    // print column name
-    //    for (int i = 0; i < PQnfields(res); i++) {
-    //        std::cout << PQfname(res, i) << "              ";
-    //    }
-    //    std::cout << std::endl;
-    //    // print column values
-    //    for (int i = 0; i < PQntuples(res); i++) {
-    //        for (int j = 0; j < PQnfields(res); j++) {
-    //            std::cout << PQgetvalue(res, i, j) << "   ";
-    //        }
-    //        std::cout << std::endl;
-    //    }
-    //}
-    //PQclear(res);
     return 0;
 }
